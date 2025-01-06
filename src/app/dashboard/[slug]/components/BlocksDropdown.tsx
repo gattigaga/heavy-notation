077d3990e411b30e882c43ee0ba083e174cdb2b9
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-01-06 22:54:46 +0700

    Show no results when search blocks results is empty

--- src/app/dashboard/[slug]/components/BlocksDropdown.tsx
+++ src/app/dashboard/[slug]/components/BlocksDropdown.tsx
@@ -76,44 +76,49 @@ const BlocksDropdown = ({
   return (
     <Popover open={isOpen} onOpenChange={onOpenChange}>
       <PopoverAnchor>{children}</PopoverAnchor>
-      <PopoverContent className="w-96 h-72 p-0" side="bottom" align="start">
-        <ScrollArea className="h-full pr-2">
-          {filteredBlockSections.map((block) => (
-            <Fragment key={block.id}>
-              <p className="font-semibold p-3 pb-1 text-sm text-zinc-400">
-                {block.title}
-              </p>
-              <div className="flex flex-col p-2">
-                {block.items.map((item) => {
-                  const Icon = item.icon;
+      <PopoverContent className="w-96 max-h-72 p-0" side="bottom" align="start">
+        {filteredBlockSections.length > 0 && (
+          <ScrollArea className="h-72 pr-2">
+            {filteredBlockSections.map((block) => (
+              <Fragment key={block.id}>
+                <p className="font-semibold p-3 pb-1 text-sm text-zinc-400">
+                  {block.title}
+                </p>
+                <div className="flex flex-col p-2">
+                  {block.items.map((item) => {
+                    const Icon = item.icon;
 
-                  return (
-                    <button
-                      key={item.id}
-                      className="p-2 rounded hover:bg-zinc-100 flex items-center gap-x-4"
-                      type="button"
-                      onClick={() => onChange?.(item.id)}
-                    >
-                      <div className="flex items-center gap-x-4">
-                        <div className="w-16 h-16 rounded border flex items-center justify-center bg-white">
-                          <Icon size={40} />
+                    return (
+                      <button
+                        key={item.id}
+                        className="p-2 rounded hover:bg-zinc-100 flex items-center gap-x-4"
+                        type="button"
+                        onClick={() => onChange?.(item.id)}
+                      >
+                        <div className="flex items-center gap-x-4">
+                          <div className="w-16 h-16 rounded border flex items-center justify-center bg-white">
+                            <Icon size={40} />
+                          </div>
+                          <div>
+                            <p className="text-lg font-medium text-left">
+                              {item.title}
+                            </p>
+                            <p className="text-zinc-400 text-left text-sm">
+                              {item.description}
+                            </p>
+                          </div>
                         </div>
-                        <div>
-                          <p className="text-lg font-medium text-left">
-                            {item.title}
-                          </p>
-                          <p className="text-zinc-400 text-left text-sm">
-                            {item.description}
-                          </p>
-                        </div>
-                      </div>
-                    </button>
-                  );
-                })}
-              </div>
-            </Fragment>
-          ))}
-        </ScrollArea>
+                      </button>
+                    );
+                  })}
+                </div>
+              </Fragment>
+            ))}
+          </ScrollArea>
+        )}
+        {filteredBlockSections.length === 0 && (
+          <div className="p-3 text-sm text-zinc-400">No results found.</div>
+        )}
       </PopoverContent>
     </Popover>
   );
