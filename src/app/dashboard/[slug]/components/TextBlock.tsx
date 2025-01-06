649518a1419d1729cc0717245d993fa45031a9c7
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-01-07 01:58:44 +0700

    Fix TextBlock component functionality

--- src/app/dashboard/[slug]/components/TextBlock.tsx
+++ src/app/dashboard/[slug]/components/TextBlock.tsx
@@ -1,44 +1,65 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 import BlockControls from "./BlockControls";
 import BlocksDropdown from "./BlocksDropdown";
 
 type Props = {
+  defaultValue: string;
   onChange?: (value: string) => void;
   onClickPlus?: () => void;
   onClickAltPlus?: () => void;
   onClickGrip?: () => void;
+  onBlockSelected?: (blockId: string) => void;
 };
 
 const TextBlock = ({
+  defaultValue,
   onChange,
   onClickPlus,
   onClickAltPlus,
   onClickGrip,
+  onBlockSelected,
 }: Props) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(defaultValue);
   const [isBlocksOpen, setIsBlocksOpen] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const refTextarea = useRef<HTMLTextAreaElement>(null);
+
+  const placeholder = isFocused
+    ? "Write something, or press '/' for commands..."
+    : "";
+
+  // Refocus to the textarea when the dropdown is closed.
+  useEffect(() => {
+    if (!isBlocksOpen && refTextarea.current) {
+      refTextarea.current.focus();
+    }
+  }, [isBlocksOpen]);
 
   return (
-    <div className="relative group">
-      <BlockControls
-        onClickPlus={onClickPlus}
-        onAltClickPlus={onClickAltPlus}
-        onClickGrip={onClickGrip}
-      />
+    <div className="group relative">
+      {!isBlocksOpen && (
+        <BlockControls
+          onClickPlus={onClickPlus}
+          onAltClickPlus={onClickAltPlus}
+          onClickGrip={onClickGrip}
+        />
+      )}
 
       <BlocksDropdown
         isOpen={isBlocksOpen}
-        onChange={onChange}
+        onChange={onBlockSelected}
         onOpenChange={setIsBlocksOpen}
       >
         <TextareaAutosize
-          className="w-full resize-none outline-none text-lg font-medium"
+          ref={refTextarea}
+          className="w-full resize-none text-lg font-medium outline-none"
           value={value}
-          placeholder="Write something, or press '/' for commands..."
+          placeholder={placeholder}
+          onFocus={() => setIsFocused(true)}
           onChange={(event) => {
             const newValue = event.target.value;
 
@@ -46,6 +67,16 @@ const TextBlock = ({
 
             if (!value && newValue.startsWith("/")) {
               setIsBlocksOpen(true);
+            }
+          }}
+          onBlur={() => {
+            setIsFocused(false);
+            onChange?.(value);
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              event.preventDefault();
+              onChange?.(value);
             }
           }}
         />
