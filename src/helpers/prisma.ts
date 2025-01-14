51d146f3cf7bcc64f80d4a25a8189a239fce91cb
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-01-14 23:19:00 +0700

    Setup base next-auth config with prisma

--- src/helpers/prisma.ts
+++ src/helpers/prisma.ts
@@ -8,6 +8,13 @@ const libsql = createClient({
 });
 
 const adapter = new PrismaLibSQL(libsql);
+
+// To improve performance using Prisma ORM,
+// we can set up the Prisma instance to ensure only one instance
+// is created throughout the project and then import it
+// from any file as needed. This approach avoids recreating instances
+// of PrismaClient every time it is used.
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
