00e0e183b11cb61c6a3ac76bb70d51e4927569f0
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-01-23 02:39:49 +0700

    Create some route handlers

--- src/app/api/pages/[slug]/route.ts
+++ src/app/api/pages/[slug]/route.ts
@@ -1,13 +1,42 @@
+import { auth } from "@/helpers/auth";
 import { prisma } from "@/helpers/prisma";
+
+export const GET = async (
+  request: Request,
+  { params }: { params: Promise<{ slug: string }> },
+) => {
+  const session = await auth();
+  const slug = (await params).slug;
+
+  const page = await prisma.page.findFirst({
+    where: {
+      userId: session?.user.id,
+      slug: slug,
+    },
+  });
+
+  if (!page) {
+    return Response.json(
+      { message: "Page is not found." },
+      {
+        status: 404,
+      },
+    );
+  }
+
+  return Response.json({ data: page });
+};
 
 export const DELETE = async (
   request: Request,
   { params }: { params: Promise<{ slug: string }> },
 ) => {
+  const session = await auth();
   const slug = (await params).slug;
 
   const page = await prisma.page.findFirst({
     where: {
+      userId: session?.user.id,
       slug: slug,
     },
   });
