dc6cd6f49b878910dece32948e24264fcf464e68
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-01-28 22:48:26 +0700

    Create home page layout for desktop viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,26 +16,26 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-24 px-8 pt-16 md:gap-y-40 md:px-16">
+    <div className="flex max-w-screen-2xl flex-col gap-y-24 px-8 pt-16 md:gap-y-40 md:px-16 lg:px-24 lg:pt-24 2xl:mx-auto 2xl:px-40">
       <div className="flex flex-col">
-        <h1 className="mb-4 whitespace-pre text-5xl font-bold tracking-tighter text-zinc-700 md:text-center md:text-7xl">
+        <h1 className="mb-4 whitespace-pre text-5xl font-bold tracking-tighter text-zinc-700 md:text-center md:text-7xl lg:text-left">
           The happier{"\n"}workspace
         </h1>
-        <p className="mb-4 text-2xl font-medium text-zinc-700 md:text-center">
-          Think. Write. Plan your ideas with a better way.
+        <p className="mb-4 whitespace-pre text-2xl font-medium text-zinc-700 md:text-center lg:text-left">
+          Think. Write. Plan your ideas{"\n"}in a better way.
         </p>
         <Button
-          className="mb-8 mt-4 h-12 w-full bg-blue-500 text-base font-semibold md:w-fit md:self-center"
+          className="mb-8 mt-4 h-12 w-full bg-blue-500 text-base font-semibold md:w-fit md:self-center lg:mb-8 lg:w-auto lg:self-start"
           type="button"
         >
-          Get Started
+          Start Writing Now
         </Button>
 
-        <p className="mb-2 text-base text-zinc-400 md:text-center">
+        <p className="mb-2 text-base text-zinc-400 md:text-center lg:text-left">
           Trusted by teams at
         </p>
-        <div className="mb-8 grid grid-cols-2 gap-4 md:grid-cols-4">
-          <div className="col-span-1 flex items-center gap-x-2 md:col-start-2">
+        <div className="mb-8 flex flex-wrap gap-4 md:justify-center lg:justify-start lg:gap-8">
+          <div className="flex items-center gap-x-2">
             <LogoDuckDuckGo
               className="shrink-0"
               width={24}
@@ -44,7 +44,7 @@ export default function Home() {
             />
             <p className="text-base font-semibold text-zinc-400">DuckDuckGo</p>
           </div>
-          <div className="col-span-1 flex items-center gap-x-2">
+          <div className="flex items-center gap-x-2">
             <LogoTwitch
               className="shrink-0"
               width={24}
@@ -53,7 +53,7 @@ export default function Home() {
             />
             <p className="text-base font-semibold text-zinc-400">Twitch</p>
           </div>
-          <div className="col-span-1 flex items-center gap-x-2 md:col-start-2">
+          <div className="flex items-center gap-x-2">
             <LogoShopify
               className="shrink-0"
               width={24}
@@ -62,7 +62,7 @@ export default function Home() {
             />
             <p className="text-base font-semibold text-zinc-400">Shopify</p>
           </div>
-          <div className="col-span-1 flex items-center gap-x-2">
+          <div className="flex items-center gap-x-2">
             <LogoDiscord
               className="shrink-0"
               width={24}
@@ -73,7 +73,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="aspect-video w-full bg-zinc-200" />
+        <div className="aspect-video w-full rounded-lg bg-zinc-200" />
       </div>
 
       <div>
@@ -84,7 +84,7 @@ export default function Home() {
           Capture your ideas, get feedback from teammates, and ask AI to add the
           finishing touches.
         </p>
-        <div className="mb-8 aspect-video w-full bg-zinc-200" />
+        <div className="mb-8 aspect-video w-full rounded-lg bg-zinc-200" />
         <div className="flex flex-wrap gap-x-4 gap-y-3">
           <p className="w-full shrink-0 text-base font-semibold text-zinc-700 md:w-auto">
             Replaces
@@ -108,7 +108,7 @@ export default function Home() {
           All your projects, goals, calendars, roadmaps, and more—in one
           tool—personalized to how you and your team work.
         </p>
-        <div className="mb-8 aspect-video w-full bg-zinc-200" />
+        <div className="mb-8 aspect-video w-full rounded-lg bg-zinc-200" />
         <div className="flex flex-wrap gap-x-4 gap-y-3">
           <p className="w-full shrink-0 text-base font-semibold text-zinc-700 md:w-auto">
             Replaces
@@ -133,7 +133,7 @@ export default function Home() {
           looking for, whether its stored in Heavy Notation or one of your other
           apps.
         </p>
-        <div className="mb-8 aspect-video w-full bg-zinc-200" />
+        <div className="mb-8 aspect-video w-full rounded-lg bg-zinc-200" />
         <div className="flex flex-wrap gap-x-4 gap-y-3">
           <p className="w-full shrink-0 text-base font-semibold text-zinc-700 md:w-auto">
             Replaces
