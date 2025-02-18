4319fdf0f6be61bdb28e3cc16ad3c638766fa4ae
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-02-18 22:39:42 +0700

    Update project name for Sentry

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const finalConfig = withSentryConfig(nextConfig, {
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
   org: process.env.SENTRY_ORG,
-  project: "javascript-nextjs",
+  project: "heavy-notation",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
