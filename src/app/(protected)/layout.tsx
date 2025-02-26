3f1834e058e5903cb8c99db22dad0bbed91806d2
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-02-27 00:44:58 +0700

    Update layout and setup provider for protected routes

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,21 @@
 import { redirect } from "next/navigation";
+import { Geist, Geist_Mono } from "next/font/google";
 
 import AppSidebar from "./components/AppSidebar";
+import Provider from "./components/Provider";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
 import { auth } from "@/helpers/auth";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 type Props = {
   children: React.ReactNode;
@@ -18,10 +31,19 @@ const Layout = async ({ children }: Props) => {
   }
 
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Provider>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>{children}</SidebarInset>
+          </SidebarProvider>
+        </Provider>
+        <Toaster />
+      </body>
+    </html>
   );
 };
 
