66c87593fa8a5e5a0583ba5dbfa9cd4a0baf51d1
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-03-05 17:42:50 +0700

    Add i18n for metadata of home page, sign in page, and sign up page

--- src/app/[lang]/auth/signup/page.tsx
+++ src/app/[lang]/auth/signup/page.tsx
@@ -1,29 +1,43 @@
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { setI18n } from "@lingui/react/server";
+import { t } from "@lingui/core/macro";
 
 import { auth } from "@/helpers/auth";
 import SignUpTemplate from "@/app/components/SignUpTemplate";
 import { getI18nInstance } from "@/app/helpers/i18n";
 
-export const metadata: Metadata = {
-  title: "Sign Up | Heavy Notation",
-  description: "Create a new Heavy Notation account.",
-  openGraph: {
-    title: "Sign Up | Heavy Notation",
-    description: "Create a new Heavy Notation account.",
-    type: "website",
-    images: [
-      {
-        url: "/images/home/screenshot.webp",
-        type: "image/webp",
-        width: 1665,
-        height: 951,
-        alt: "Heavy Notation website homepage",
-      },
-    ],
-    url: "https://heavynotation.vercel.app/auth/signup",
-  },
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const lang = (await params).lang;
+  const i18n = getI18nInstance(lang);
+
+  const title = t(i18n)`Sign Up` + " | Heavy Notation";
+  const description = t(i18n)`Create a new Heavy Notation account.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "website",
+      images: [
+        {
+          url: "/images/home/screenshot.webp",
+          type: "image/webp",
+          width: 1665,
+          height: 951,
+          alt: "Heavy Notation website homepage",
+        },
+      ],
+      url:
+        lang !== "en"
+          ? `https://heavynotation.vercel.app/${lang}/auth/signup`
+          : "https://heavynotation.vercel.app/auth/signup",
+    },
+  };
 };
 
 type Props = {
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,31 +1,47 @@
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { setI18n } from "@lingui/react/server";
+import { t } from "@lingui/core/macro";
 
 import { auth } from "@/helpers/auth";
 import HomeTemplate from "../components/HomeTemplate";
 import { getI18nInstance } from "../helpers/i18n";
 
-export const metadata: Metadata = {
-  title: "Your workspace solution for projects | Heavy Notation",
-  description:
-    "A new tool that blends your everyday work apps into one. It's the all-in-one workspace for you.",
-  openGraph: {
-    title: "Your workspace solution for projects | Heavy Notation",
-    description:
-      "A new tool that blends your everyday work apps into one. It's the all-in-one workspace for you.",
-    type: "website",
-    images: [
-      {
-        url: "/images/home/screenshot.webp",
-        type: "image/webp",
-        width: 1665,
-        height: 951,
-        alt: "Heavy Notation website homepage",
-      },
-    ],
-    url: "https://heavynotation.vercel.app",
-  },
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const lang = (await params).lang;
+  const i18n = getI18nInstance(lang);
+
+  const title =
+    t(i18n)`Your workspace solution for projects` + "| Heavy Notation";
+
+  const description = t(
+    i18n,
+  )`A new tool that blends your everyday work apps into one. It's the all-in-one workspace for you.`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "website",
+      images: [
+        {
+          url: "/images/home/screenshot.webp",
+          type: "image/webp",
+          width: 1665,
+          height: 951,
+          alt: "Heavy Notation website homepage",
+        },
+      ],
+      url:
+        lang !== "en"
+          ? `https://heavynotation.vercel.app/${lang}`
+          : "https://heavynotation.vercel.app",
+    },
+  };
 };
 
 type Props = {
