ca5375c8f197b46861caba0e1d721f32b7016657
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2025-03-18 00:12:44 +0700

    Make index page structure more semantic

--- src/app/components/HomeTemplate.tsx
+++ src/app/components/HomeTemplate.tsx
@@ -69,8 +69,6 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
 
   return (
     <>
-      <GoogleTagScript />
-
       <header className="flex h-16 items-center justify-between px-4">
         <Link href={lang !== "en" ? `/${lang}` : "/"}>
           <Image
@@ -93,8 +91,8 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
         </Button>
       </header>
 
-      <div className="flex max-w-screen-2xl flex-col gap-y-24 px-8 pt-4 md:gap-y-40 md:pt-10 lg:pt-16 2xl:mx-auto">
-        <div className="flex flex-col md:px-16 lg:px-24 2xl:px-40">
+      <main className="flex max-w-screen-2xl flex-col gap-y-24 px-8 pt-4 md:gap-y-40 md:pt-10 lg:pt-16 2xl:mx-auto">
+        <section className="flex flex-col md:px-16 lg:px-24 2xl:px-40">
           <TypingAnimation
             className="mb-4 text-center text-5xl font-bold tracking-tighter text-zinc-700 md:text-7xl dark:text-white"
             as="h1"
@@ -132,9 +130,9 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
             <source src="/videos/home/demo.mov" type="video/mp4" />
             <Trans>Your browser does not support the video tag.</Trans>
           </video>
-        </div>
+        </section>
 
-        <div className="md:px-16 lg:px-24 2xl:px-40">
+        <section className="md:px-16 lg:px-24 2xl:px-40">
           <h2 className="mb-4 text-center text-4xl font-bold text-zinc-700 md:text-6xl dark:text-white">
             <Trans>Build perfect docs.</Trans>
           </h2>
@@ -157,9 +155,9 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
             />
           </picture>
           <Replacement items={["evernote", "coda"]} />
-        </div>
+        </section>
 
-        <div className="md:px-16 lg:px-24 2xl:px-40">
+        <section className="md:px-16 lg:px-24 2xl:px-40">
           <h2 className="mb-4 text-center text-4xl font-bold text-zinc-700 md:text-6xl dark:text-white">
             <Trans>Your workflow. Your way.</Trans>
           </h2>
@@ -182,9 +180,9 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
             />
           </picture>
           <Replacement items={["trello", "asana"]} />
-        </div>
+        </section>
 
-        <div className="md:px-16 lg:px-24 2xl:px-40">
+        <section className="md:px-16 lg:px-24 2xl:px-40">
           <h2 className="mb-4 text-center text-4xl font-bold text-zinc-700 md:text-6xl dark:text-white">
             <Trans>Find everything. Instantly.</Trans>
           </h2>
@@ -207,9 +205,9 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
             />
           </picture>
           <Replacement items={["confluence", "asana"]} />
-        </div>
+        </section>
 
-        <div className="flex w-full flex-col">
+        <section className="flex w-full flex-col">
           <h2 className="mb-4 w-1/2 self-center text-center text-4xl font-bold text-zinc-700 md:text-6xl dark:text-white">
             <Trans>Don&apos;t just take our word for it.</Trans>
           </h2>
@@ -239,9 +237,9 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
               and do not reflect the actual experience of all users.
             </Trans>
           </p>
-        </div>
+        </section>
 
-        <div className="flex flex-col items-center rounded-lg bg-zinc-100 p-8 md:p-16 lg:p-24 dark:bg-zinc-800">
+        <section className="flex flex-col items-center rounded-lg bg-zinc-100 p-8 md:p-16 lg:p-24 dark:bg-zinc-800">
           <h2 className="mb-4 whitespace-pre text-center text-4xl font-bold text-zinc-700 md:text-6xl dark:text-white">
             <Trans>Get Started.</Trans>
           </h2>
@@ -262,8 +260,8 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
               <Trans>Start Writing Now</Trans>
             </Link>
           </Button>
-        </div>
-      </div>
+        </section>
+      </main>
 
       <footer className="flex max-w-screen-2xl flex-col items-center gap-y-2 px-8 py-8 md:h-16 md:flex-row md:justify-between md:px-8 md:py-0 2xl:mx-auto">
         <p className="text-center text-sm text-zinc-700 dark:text-white">
@@ -291,6 +289,8 @@ const HomeTemplate = ({ lang = "en" }: Props) => {
           </Link>
         </div>
       </footer>
+
+      <GoogleTagScript />
     </>
   );
 };
